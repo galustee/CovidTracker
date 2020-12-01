36deaf6f4e92f0ef7b07c694f615d64f967ab056
Peeble <peebletheory@gmail.com>
2020-11-30 20:47:38 -0800

    Pins with info added to map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { MapContainer, Circle, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, Circle, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Leaflet = (props) => {
-        const position = [37.7749, -122.4194];
-        const zoom = 13;
+        let position = [0, 0];
+        let confirmed = 0;
+        const zoom = 2;
         const data = props.data;
+        const items = [];
         console.log('test');
         //console.log(props.data)
 
         for (let key in data) {
             if (data[key].lat != null) {
-                console.log(data[key].lat, data[key].long);
+                position = [data[key].lat, data[key].long];
+                let confirmed = data[key].confirmed;
+                let deaths = data[key].deaths;
+                let recovered = data[key].recovered;
+                console.log(position);
+                items.push(
+                <Marker position={position} key={key}>
+                        <Popup>
+                            {key} <br />
+                            Confirmed: {confirmed} <br />
+                            Recovered: {recovered} <br />
+                            Deaths: {deaths} <br />
+
+                        </Popup>
+                </Marker>);
             }
         }
 
@@ -21,8 +37,8 @@ const Leaflet = (props) => {
                         attribution={'&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
                         />
                         <Marker position={position}>
-
                         </Marker>
+                        {items}
             </MapContainer>
         )
 }
