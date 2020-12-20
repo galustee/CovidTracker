8e187ebe4dfbda4a249e8248e79e9df8ca6a65e8
Peeble <peebletheory@gmail.com>
2020-12-19 18:10:00 -0800

    california counties

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import {Line, Bar } from 'react-chartjs-2';
+import {Line, Bar, Polar, Doughnut } from 'react-chartjs-2';
 
 import styles from './Charts.module.css';
 
 const Charts = (props) => {
 
     let la_county;
+    let countyarray = [];
+    let countyconfirmed = [];
+    let countynames = [];
+    let californiaconfirmed = []
+    let californianames = []
+    let californiacolors = []
+
+    // from https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript
+    function random_rgba() {
+        var o = Math.round, r = Math.random, s = 255;
+        return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+    }
+
+
 
     console.log('doodoo')
     if (props.data.cityData[0] != null) {
@@ -15,6 +29,18 @@ const Charts = (props) => {
             if (props.data.cityData[i].location === "Los Angeles County, California") {
                 console.log(i);
                 la_county = i;
+            }
+            if (props.data.cityData[i].country_code.includes('us')) {
+                countyarray.push(props.data.cityData[i])
+                countyconfirmed.push(props.data.cityData[i].confirmed)
+                countynames.push(props.data.cityData[i].location)
+                //console.log(countyarray);
+                if (props.data.cityData[i].location.includes('California')) {
+                    californiaconfirmed.push(props.data.cityData[i].confirmed)
+                    californianames.push(props.data.cityData[i].location)
+                    californiacolors.push(random_rgba())
+                    console.log(californiacolors)
+                }
             }
         }
     }
@@ -41,9 +67,33 @@ const Charts = (props) => {
         ) : null
 
     )
+
+    const doughtnutChart = (
+        props.data.cityData[0] ? (
+            <Doughnut
+            data={{
+                labels: californianames,
+                datasets: [{
+                    label: 'People',
+                    backgroundColor: californiacolors,
+                    data: californiaconfirmed
+                }]
+            }}
+            options={{
+                legend: {display: false},
+                title: {display: true, text:`California Counties`},
+            }}
+            />
+        ) : null
+    )
     return(
         <div className={styles.container}>
+            <div className ={styles.charts}>
             {barChart}
+            </div>
+            <div className ={styles.charts}>
+            {doughtnutChart}
+            </div>
         </div>
     )
 }
