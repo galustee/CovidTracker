958b0dee80acc3fb9c4f25c95c976f294c740b6c
GreyPi <47376447+GreyPi@users.noreply.github.com>
2020-12-20 18:26:09 -0800

    Updated final time to include city function

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -3,7 +3,7 @@ import { NativeSelect, FormControl, StylesProvider } from '@material-ui/core';
 
 import styles from './SearchBar.module.css';
 
-import { searchCountries } from '../../api';
+import { searchCountries, searchCities } from '../../api';
 
 const SearchBar = ( {handleCountryChange} ) => {
     const [searchedCountries, setSearchedCountries] = useState([]);
@@ -19,7 +19,7 @@ const SearchBar = ( {handleCountryChange} ) => {
     // console.log(searchedCountries);
     
     return (
-        <FormControl className = {StylesProvider.formControl}>
+        <FormControl className = {styles.formControl}>
             <NativeSelect defaultValue = '' onChange = {(e) => handleCountryChange(e.target.value)}>
                 <option value = 'countries'>Countries</option>
                 {searchedCountries.map((country, i) => <option key = {i} value = {country}>{country}</option>)}
@@ -28,4 +28,27 @@ const SearchBar = ( {handleCountryChange} ) => {
     )
 }
 
-export default SearchBar;
+const SearchBar1 = ( {handleCityChange} ) => {
+    const [searchedCities, setSearchedCities] = useState([]);
+
+    useEffect(() => {
+        const fetchCities = async () => {
+            setSearchedCities(await searchCities());
+        }
+
+        fetchCities();
+    }, [setSearchedCities]);
+    
+    // console.log(searchedCountries);
+    
+    return (
+        <FormControl className = {styles.formControl}>
+            <NativeSelect defaultValue = '' onChange = {(e) => handleCityChange(e.target.value)}>
+            <option value = 'cities'>Cities</option>
+            {searchedCities.map((city, j) => <option key = {j} value = {city}>{city}</option>)}
+            </NativeSelect>
+        </FormControl>
+    )
+}
+
+export {SearchBar as default, SearchBar1};
