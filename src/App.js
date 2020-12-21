6093e32fa4292404b72314db6fd63ece02561f52
GreyPi <47376447+GreyPi@users.noreply.github.com>
2020-12-20 16:18:27 -0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 //import { Data, Leaflet } from './components';
-import { Leaflet, Charts } from './components';
+import { Leaflet, Charts, SearchBar } from './components';
 import styles from './App.module.css';
 
-import { fetchCountry, fetchProvince, fetchCity, fetchHistorical, fetchUSHistorical} from './api'
-import CountryList from './components/Search/CountryList';
-import SearchBar from './components/Search/SearchBar';
+import { fetchCountry, fetchProvince, fetchCity, fetchHistorical, fetchUSHistorical, searchCountries} from './api';
 
 
 class App extends React.Component{
@@ -14,9 +12,7 @@ class App extends React.Component{
         countryData: {},
         provinceData: {},
         cityData: {},
-        stats:[],
-        searchField: ''
-
+        country: '',
     }
 
     async componentDidMount() {
@@ -25,12 +21,14 @@ class App extends React.Component{
         const cityData = await fetchCity();
         const historicalData = await fetchHistorical();
         const UShistoricalData = await fetchUSHistorical();
+        const searchCountryData = await searchCountries();
 
         console.log(countryData);
         console.log(provinceData);
         console.log(cityData);
         console.log(historicalData);
         console.log(UShistoricalData);
+        console.log(searchCountryData);
 
         //const { data } = fetchedData;
 
@@ -41,32 +39,19 @@ class App extends React.Component{
           
         // }
 
-        const resp = await fetch('https://api.covid19api.com/countries')
-        const countries = await resp.json()
-        this.setState({countries})
-        this.state.countries.forEach(async country => {
-            const resp = await fetch(`https://api.covid19api.com/total/country/${country.Slug}`)
-            const data = await resp.json()
-            if(data.length)
-            this.setState(prevState => (
-                {stats:prevState.stats.concat({...data[data.length - 1],CountryCode:country.ISO2})}
-      ))
-    })
-
-        this.setState({countryData, provinceData, cityData, UShistoricalData});
+        this.setState({countryData, provinceData, cityData, UShistoricalData, searchCountryData});
         //console.log(this.state);
         //console.log(this.state);
     }
 
-    handleChange = (e) =>{
-        this.setState({searchField:e.target.value})
-      }
+    handleCountryChange = async (country) => {
+        //console.log(country);
+        const fetchedCountry = await fetchCountry(country);
+        console.log(fetchedCountry)
+        //this.setState({ data: fetchedCountry, country: country});
+    }
 
     render() {
-        const {stats,searchField} = this.state
-        const filteredCountries = stats.filter(country =>(
-        country.Country.toLowerCase().includes(searchField.toLowerCase())
-            ))
         //console.log('boop');
         const data = this.state;
         //console.log(data);
@@ -74,9 +59,8 @@ class App extends React.Component{
         return (
             <div className="stuff">
                 <Leaflet data={data}/>
-                <Charts data={data}/>
-                <SearchBar placeholder = 'Enter Country Name' handleChange = {this.handleChange}/>
-                <CountryList stats = {filteredCountries}/>
+                <Charts data={data} />
+                <SearchBar handleCountryChange = {this.handleCountryChange}/>
             </div>
         )
         //<Data />
